037178a43f5d7636f80f6a1a4c37b90bef3f7dc1
subinjeong123 <55428469+subinjeong123@users.noreply.github.com>
2019-10-15 15:46:34 +0900

    06-6

--- main.c
+++ main.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop*/
 
-int square(int n);
 int main(void)
 {
-	int result;
-	result = square(5);
-	printf("%d", result);
+	int i;
+	int max=45;
 	
-	}
-	int square(int n)
+	srand((unsigned) time(NULL) );
+	for (i = 0; i < 6; i++ ) 
 	{
-		return (n*n);
+		printf("%d " , 1+rand()%max); 
+	}
+		return 0;
 	}
